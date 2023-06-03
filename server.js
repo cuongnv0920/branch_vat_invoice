4400200e27ea822c1fcc55b5bb24b2c51cc1f3de
unknown <cuongnv0920@gmail.com>
2023-06-03 15:14:23 +0700

    init

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const app = express();
+const port = 5003;
+
+// routes
+
+// Connect database mongodb
+
+// use routes
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
