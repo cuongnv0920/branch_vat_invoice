7fce5101ab687f7ee5dec340ebc7ea39704b586f
NGUYEN VAN CUONG <cuongnv12@BIDVBANK.bidv.com>
2023-06-07 16:43:47 +0700

    0706

--- server.js
+++ server.js
@@ -5,10 +5,36 @@ const app = express();
 const port = 5003;
 
 // routes
+const authRoute = require("./routes/auth.route");
+const userRoute = require("./routes/user.route");
+const levelRoute = require("./routes/level.route");
+const roomRoute = require("./routes/room.route");
 
 // Connect database mongodb
+main()
+  .then(() => console.log("database connected."))
+  .catch((err) => console.log(err));
+async function main() {
+  await mongoose.set("strictQuery", true);
+  await mongoose.connect("mongodb://127.0.0.1:27017/branch_applications");
+}
+
+var corsOptions = {
+  origin: "*",
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
 
 // use routes
+app.use("/auth", authRoute);
+app.use("/room", roomRoute);
+app.use("/user", userRoute);
+app.use("/level", levelRoute);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
