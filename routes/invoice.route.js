42fee0a45baaa03017516b40aabc80ed8eb278ff
unknown <cuongnv0920@gmail.com>
2023-06-15 21:59:43 +0700

    1506

--- routes/invoice.route.js
+++ routes/invoice.route.js
@@ -2,9 +2,10 @@ const express = require("express");
 const router = express.Router();
 const validator = require("../validators/invoice.validator");
 const controller = require("../controllers/invoice.controller");
+const uploadReadXml = require("../config/storageCacheConf");
 
 // router.get("/getAll", controller.getAll);
-router.post("/readXml", controller.readXml);
+router.post("/readXml", uploadReadXml.single("file_1"), controller.readXml);
 // router.get("/get/:id", controller.get);
 // router.post("/create", validator.validatorCreate(), controller.create);
 // router.put("/update/:id", validator.validatorUpdate(), controller.update);
